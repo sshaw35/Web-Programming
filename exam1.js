40bea958f5cbf0b35716c8ec1e89e79a3ef80b1e
sshaw35 <157416021+sshaw35@users.noreply.github.com>
2025-02-18 11:42:09 -0500

    Add files via upload

---
+++ exam1.js
@@ -0,0 +1,7 @@
+document.getElementById('search').addEventListener('click', function() {
+    alert('Search button clicked');
+});
+
+document.getElementById('lucky').addEventListener('click', function() {
+    alert("Lucky button clicked");
+});
